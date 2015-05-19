f1319b42da2e8e15919827ddd95f9e9856d0fbc7
robulus112 <Robin@192.168.1.40>
2015-05-19 12:47:05 +0200

    Geändert: Login, UserList, AccesData

---
+++ src/p_case/UserList.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p_case;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Robin
+ */
+public class UserList {
+   List <String> userData = new ArrayList<String>();
+ 
+    
+}
